ca030dd0e0dac8a1353e6023c499167050e391cd
Shishira Ramesh <Shishira@Shishiras-MacBook-Pro.local>
2014-07-20 20:06:11 -0400

    Initial Commit

---
+++ SavantCareers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SavantCareers
+//
+//  Created by Shishira Ramesh on 7/20/14.
+//  Copyright (c) 2014 Shishira Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SavantCareers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SavantCareers
+//
+//  Created by Shishira Ramesh on 7/20/14.
+//  Copyright (c) 2014 Shishira Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
